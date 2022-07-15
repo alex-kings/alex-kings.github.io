eb54941662646ab4fc7cdc48b24b09d2f2310799
alex-kings <alexandrekings1@gmail.com>
2022-07-15 11:11:49 +0100

    Modified background.

--- Main.js
+++ Main.js
@@ -1,4 +1,6 @@
 import * as THREE from "https://cdn.skypack.dev/three@0.133.1";
+import {OBJLoader} from "./OBJLoader.js";
+import {OrbitControls} from "./OrbitControls.js";
 
 // Scene, camera and renderer setup
 const scene = new THREE.Scene();
@@ -6,23 +8,50 @@ const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHei
 const renderer = new THREE.WebGLRenderer({canvas:document.getElementById("canvas")});
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
-camera.position.setZ(30);
+camera.position.setZ(4);
 
+// Set up orbit controls
+const controls = new OrbitControls(camera, renderer.domElement);
 
-const obj = new THREE.Mesh(
-    new THREE.BoxGeometry(10,20,30),
-    new THREE.MeshStandardMaterial({color:0xffffff})
-)
-scene.add(obj);
-
-const light = new THREE.AmbientLight(0xff0000);
+// Set up lighting
+const light = new THREE.AmbientLight(0xFFFFFF);
 light.position.set({x:10,y:10,z:10})
 scene.add(light);
 
+// Set background
+const pastelTexture = new THREE.TextureLoader().load("./textures/pastel_background.jpg")
+scene.background = pastelTexture;
+
+// Generate a sphere at a given position. Returns the sphere.
+function generateSphere(pos, radius, colour){
+	let sphere =  new THREE.Mesh(
+		new THREE.SphereGeometry(radius),
+		new THREE.MeshStandardMaterial({color:colour})
+	)
+	sphere.position.set(...pos)
+	return sphere;
+}
+
+// Generate n spheres of radius between r1 and r2 at positions between R1 and R2
+function generateSpheres(n,r1,r2,dist){
+	let spheres = [];
+	for(let i=0;i<n;i++){
+		// Generate a random radius between r1 and r2
+		let radius = Math.random()*(r2-r1) + r1;
+		// Generate a random position between R1 and R2
+		const pos = Array(3).fill().map(()=> THREE.MathUtils.randFloatSpread(dist));
+		spheres.push(generateSphere(pos, radius, 0xffffff))
+	}
+	return spheres;
+}
+
+const stars = generateSpheres(100, 0.1,0.2, 50);
+stars.forEach((sphere)=>scene.add(sphere));
+
+
 function render(){
-    obj.rotation.x += 0.01;
-    obj.rotation.y += 0.02;
-    obj.rotation.z += 0.03;
+
+	controls.update();
     requestAnimationFrame(render);
     renderer.render(scene,camera);
 }
